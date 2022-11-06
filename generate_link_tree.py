c886e710bba806a41c2720661bef46b54e40a475
tde-nico <101901057+tde-nico@users.noreply.github.com>
2022-11-06 16:56:06 +0100

    new field implementation

--- generate_link_tree.py
+++ generate_link_tree.py
@@ -42,6 +42,7 @@ def get_tags(username, repo):
 	content = req.content.decode()
 	comment = content.split('<!--', 1)[-1].split('-->', 1)[0]
 
+	field = []
 	groups = []
 	langs = []
 	frames_and_libs = []
@@ -50,6 +51,9 @@ def get_tags(username, repo):
 
 	reader = None
 	for line in lines:
+		if line == '#field':
+			reader = 'field'
+			continue
 		if line == '#groups':
 			reader = 'groups'
 			continue
@@ -60,6 +64,9 @@ def get_tags(username, repo):
 			reader = 'frames'
 			continue
 
+		if reader == 'field':
+			line = line.replace("_", " ")
+			field.append(line)
 		if reader == 'groups':
 			line = line.replace("_", " ")
 			groups.append(line)
@@ -68,7 +75,7 @@ def get_tags(username, repo):
 		elif reader == 'frames':
 			frames_and_libs.append(line)
 
-	return groups, langs, frames_and_libs
+	return field, groups, langs, frames_and_libs
 
 
 
@@ -121,9 +128,43 @@ def generate_section(username, name, repos, columns):
 	return text
 
 
+def generate_field(username, name, sections, columns):
+	text = f"<details>\n<summary>{name}</summary>\n<br>\n"
+
+	for sub in sections[name]:
+		if type(sub) == list:
+			text += generate_section(username, sub[0], sorted(sub[1:], key=lambda x: x['name'].lower()), columns)
+
+	text += '<table border=3 align=\"center\">\n<tr>'
+	for col in ['Name'] + list(columns.keys()):
+		text += f"""
+	<td>
+		{col}
+	</td>"""
+	text += '\n</tr>\n'
+
+	for sub in sections[name]:
+		if type(sub) == dict:
+			text += '<tr>'
+			text += f"""
+	<td>
+		<a href="https://github.com/{username}/{sub['link_name']}">{sub['name']}</a>
+	</td><td>
+		{sub['langs']}
+	</td><td>
+		{sub['frames_and_libs']}
+	</td>"""
+			text += '\n</tr>\n'
+
+	text += "</table>\n<br>\n</details>\n"
+	return text
+
+
+
 
 
 def main(username, fname, columns, verbose):
+
 	if verbose:
 		print(f'[+] Grabbing {username} repos')
 	try:
@@ -140,12 +181,12 @@ def main(username, fname, columns, verbose):
 		if verbose:
 			print(f'[+] Grabbing {repo}')
 		try:
-			groups, langs, frames_and_libs = get_tags(username, repo)
-			data[repo] = {'groups': groups, 'langs': langs, 'frames_and_libs': frames_and_libs}
+			field, groups, langs, frames_and_libs = get_tags(username, repo)
+			data[repo] = {'field': field, 'groups': groups, 'langs': langs, 'frames_and_libs': frames_and_libs}
 		except Exception:
 			if verbose:
 				print(f'[-] Error while grabbing {repo}')
-	
+
 	sections = {}
 	group = 0
 	default_section = 'Projects'
@@ -153,19 +194,37 @@ def main(username, fname, columns, verbose):
 		if verbose:
 			print(f'[+] Formatting {repo} data')
 		repo_info = get_repo_info(repo, info, columns)
-		#print(sections)
 		if info['groups']:
 			sections[info['groups'][group]] = sections.get(info['groups'][group], []) + [repo_info]
 		else:
-			#print(sections.get(default_section, []), repo_info)
 			sections[default_section] = sections.get(default_section, []) + [repo_info]
+
+	group = 0
+	for repo, info in data.items():
+		if info['field']:
+			sections[info['field'][0]] = sections.get(info['field'][0], []) + info['groups']
+			sections[info['field'][0]] = [i for n, i in enumerate(sections[info['field'][0]]) if i not in sections[info['field'][0]][:n]]
+
+	for section in list(sections.keys()):
+		if section in sections.keys():
+			for sub in sections[section]:
+				if type(sub) == str:
+					sections[section].append([sub] + sections[sub])
+					del sections[sub]
 
 	tree = '# link_tree\n\n'
 	tree += "\n<!--\n#groups\n\n#languages\nPython\n\n#frames and libs\n\n-->\n\n\n"
 	for section in sorted(sections.keys()):
 		if verbose:
 			print(f'[+] Generating {section} section')
-		tree += generate_section(username, section, sorted(sections[section], key=lambda x: x['name'].lower()), columns)
+		for sub in sections[section]:
+			if type(sub) == dict:
+				continue
+			else:
+				tree += generate_field(username, section, sections, columns)
+				break
+		else:
+			tree += generate_section(username, section, sorted(sections[section], key=lambda x: x['name'].lower()), columns)
 		tree += '\n\n'
 
 	if verbose:
